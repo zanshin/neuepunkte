4908da6f0b2bf5aaefa5f6351c849721d2c1d4ac
Mark Nichols <mark@zanshin.net>
2020-08-05 09:02:26 -0500

    catching up

--- polybar/arch/ip.sh
+++ polybar/arch/ip.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-public_ip=$(dig +short myip.opendns.com @resolver1.opendns.com)
-private_ip=$(hostname -i | awk '{print $1}')
+# public_ip=$(dig +short myip.opendns.com @resolver1.opendns.com)
+public_ip=$(curl -s https://api.ipify.org)
+# private_ip=$(hostname -i | awk '{print $1}')
+private_ip=$(ip a | grep 192.168.4 | awk '{print $2}')
 
 ip_addresses=$(echo "IPs: $public_ip $private_ip")
 
